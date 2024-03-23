2b4a4e0cf0499d55cf939ffce3e96a4406a6ac30
pjtae <158832454+pjtae@users.noreply.github.com>
2024-03-23 22:45:58 +0900

    MultiThreadtest

---
+++ Practice_MultiThreading/Practice_MultiThreading/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Practice_MultiThreading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static Barista HireBarista(string nickname)
+    {
+        return new Barista(nickname);
+    }
+}
+
+public class Barista
+{
+    public Barista(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+
+    public Barista GoToWork()
+    {
+        Console.WriteLine($"바리스타 {Name} 이 출근했습니다...");
+        return this;
+    }
+}
